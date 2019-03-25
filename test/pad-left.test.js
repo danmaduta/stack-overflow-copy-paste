2390bafba67d5c560a75e1573ab2022ec16d0185
Dan M. Maduta <mircea.dan.maduta@gmail.com>
2019-03-25 13:05:07 +0100

    I need help with the commit message

--- test/pad-left.test.js
+++ test/pad-left.test.js
@@ -1,27 +1,27 @@
-import test from 'ava'
-import {padLeft} from '../src'
+import test from "ava";
+import { padLeft } from "../src";
 
-test('pads left of the given string', t => {
-  const original = '123'
-  const expected = 'zz123'
-  const padLength = 5
-  const padWith = 'z'
-  const actual = padLeft(original, padLength, padWith)
-  t.deepEqual(actual, expected)
-})
+test("pads left of the given string", t => {
+  const original = "987";
+  const expected = "zz987";
+  const padLength = 5;
+  const padWith = "z";
+  const actual = padLeft(original, padLength, padWith);
+  t.deepEqual(actual, expected);
+});
 
-test('defaults to pad a zero', t => {
-  const original = '123'
-  const expected = '00123'
-  const padLength = 5
-  const actual = padLeft(original, padLength)
-  t.deepEqual(actual, expected)
-})
+test("defaults to pad a zero", t => {
+  const original = "987";
+  const expected = "00987";
+  const padLength = 5;
+  const actual = padLeft(original, padLength);
+  t.deepEqual(actual, expected);
+});
 
-test('does not pad a string longer than the pad length', t => {
-  const original = '1234'
-  const expected = '1234'
-  const padLength = 3
-  const actual = padLeft(original, padLength)
-  t.deepEqual(actual, expected)
-})
+test("does not pad a string longer than the pad length", t => {
+  const original = "9876";
+  const expected = "9876";
+  const padLength = 3;
+  const actual = padLeft(original, padLength);
+  t.deepEqual(actual, expected);
+});
